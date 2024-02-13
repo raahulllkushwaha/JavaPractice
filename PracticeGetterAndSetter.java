520234fa1078ed4564223940853b67da402b3ad2
Rahul Kushwaha <raahullkushwaha@gmail.com>
2024-02-13 20:05:21 +0530

    Add Rectangle class and access their member function using Constructor

--- PracticeGetterAndSetter.java
+++ PracticeGetterAndSetter.java
@@ -6,6 +6,7 @@ class myCylinder {
         this.height = height;
         this.radius = radius;
     }
+
     public void setHeight(int height) {
         this.height = height;
     }
@@ -31,14 +32,55 @@ class myCylinder {
     }
 }
 
+// Second class for Rectangle
+
+class Rectangle {
+    private int length;
+    private int breadth;
+
+    public Rectangle() {
+        this.length = 5;
+        this.breadth = 6;
+    }
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+}
+
 public class PracticeGetterAndSetter {
     public static void main(String[] args) {
-        myCylinder cyn = new myCylinder(9,5);
+        myCylinder cyn = new myCylinder(9, 5);
+        Rectangle r = new Rectangle();
+        Rectangle r1 = new Rectangle(4, 5);
         // cyn.setHeight(9);
         System.out.println("Height of the cylinder is: " + cyn.getHeight());
         // cyn.setRadius(5);
         System.out.println("Radius of the cylinder is: " + cyn.getRadius());
         System.out.println("Surface area of a Cylinder is: " + cyn.surfaceOfCylinder());
         System.out.println("Volume of a Cylinder is: " + cyn.volumeOfCylinder());
+        // r.setLength(0);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+
+        System.out.println(r1.getLength());
+        System.out.println(r1.getBreadth());
     }
 }
