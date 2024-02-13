ab3a0d7c51068e6c0afe35343496295c80d739fd
Rahul Kushwaha <raahullkushwaha@gmail.com>
2024-02-13 17:47:27 +0530

    Practing Parameterize Constructor and Default Constructor

--- Constructor.java
+++ Constructor.java
@@ -1,32 +1,45 @@
 // import java.util.Scanner;
 
-class MyEmployee{
-    private int  id;
+class MyEmployee {
+    
+    private int id;
     public String name;
 
-    public MyEmployee(){
-         id = 12;
-         name = "Rahul";
+    public MyEmployee() {
+        id = 12;
+        name = "Rahul";
     }
-    public void setName(String n){
+
+    public MyEmployee(String myName) {
+        id = 12;
+        name = myName;
+    }
+
+    public void setName(String n) {
         this.name = n;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
 }
+
 public class Constructor {
- public static void main(String[] args) {
-    MyEmployee emp = new MyEmployee();
-    System.out.println(emp.getId());
-    System.out.println(emp.getName());
- }   
+    public static void main(String[] args) {
+        MyEmployee emp1 = new MyEmployee();
+        MyEmployee emp = new MyEmployee("Rahul Kushwaha");
+        System.out.println(emp.getId());
+        System.out.println(emp.getName());
+
+        System.out.println(emp1.getId());
+        System.out.println(emp1.getName());
+    }
 }
