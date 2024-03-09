60d368583c0f598b3d47c0df2fb12694852f1b04
Rahul Kushwaha <raahullkushwaha@gmail.com>
2024-03-09 05:48:36 +0530

    Practicing for class test array and object

---
+++ cls.java
@@ -0,0 +1,19 @@
+public class cls {
+    public static void main(String[] args) {
+        // int arr[]=new int[5];
+        // arr[0]= 12;
+        // arr[1]=56;
+        // arr[2]=30;
+        // arr[3]=30;
+        // arr[4]=30;
+        // for(int i=0; i<=arr.length; i++){
+        //     System.out.println("Elements at Index: " + i + ": " + arr[i]);
+        // }
+
+        // int arr1[]={1,2,3,4,5};
+        // // accessing the elements of the specified array
+        // for (int i = 0; i < arr1.length; i++)
+        //     System.out.println("Element at index " + i
+        //                        + " : " + arr1[i]);
+    }
+}
