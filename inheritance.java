b8ed5201a86952d592a8e4c6fe9d60dd0b8e8e16
Rahul Kushwaha <raahullkushwaha@gmail.com>
2024-03-12 19:14:09 +0530

    Practicing Inheritance

---
+++ inheritance.java
@@ -0,0 +1,50 @@
+class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void eat() {
+        System.out.println(name + " is eating...");
+    }
+
+    void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+}
+
+class Dog extends Animal {
+
+    Dog(String name){
+        super(name);
+    }
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name){
+        super(name);
+    }
+
+    void meow(){
+        System.out.println(name + " is meowing");
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog("Nax");
+        Cat c = new Cat("kitten");
+        
+        d.eat();
+        d.sleep();
+        d.bark();
+
+        c.sleep();
+        c.meow();
+    }
+}
